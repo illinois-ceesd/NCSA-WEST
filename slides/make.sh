6cf94ca38b7ce4412de58c3a277859f8dbb5b466
Andreas Kloeckner <inform@tiker.net>
2023-09-13 10:37:43 -0500

    Cobble together some drafty slides

---
+++ slides/make.sh
@@ -0,0 +1,31 @@
+#! /bin/bash
+
+set -e
+
+export TEXINPUTS="$TEXINPUTS:media:figures:ceesd-style"
+
+unset PYTHONWARNINGS
+
+DOWATCH="0"
+
+if [[ "$1" = "watch" ]]; then
+  DOWATCH="1"
+  shift
+fi
+
+if [[ "$1" = "" ]]; then
+  FILES=(talk.tex )
+else
+  FILES=("$@")
+fi
+
+#LATEXRUN="python -m pudb $(which latexrun) --latex-cmd lualatex --bibtex-cmd $BIBER --verbose-cmds -O out"
+LATEXRUN="latexrun --latex-cmd pdflatex --verbose-cmds -O out"
+
+if [[ "$DOWATCH" = "1" ]]; then
+  git ls-files | entr $LATEXRUN "${FILES[0]}"
+else
+  for f in "${FILES[@]}"; do
+    $LATEXRUN "$f"
+  done
+fi
